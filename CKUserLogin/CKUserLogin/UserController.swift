7bd1cc4357c3e8608c41783521b35d1c70fed01e
Lo <lolearningcode@gmail.com>
2019-06-05 11:20:47 -0600

    finished mode and model controller

--- CKUserLogin/CKUserLogin/UserController.swift
+++ CKUserLogin/CKUserLogin/UserController.swift
@@ -20,13 +20,31 @@ class UserController {
     
     func createNewUser(username: String, firstName: String, completion: @escaping (Bool) -> Void) {
         CKContainer.default().fetchUserRecordID { (appleUserID, error) in
+            
             if let error = error {
                 print("Fetch error \(error.localizedDescription)")
                 completion(false); return
             }
+            
             guard let appleUserID = appleUserID else { completion(false); return }
+            
             let appleUserReference = CKRecord.Reference(recordID: appleUserID, action: .deleteSelf)
+            
             let newUser = User(username: username, firstName: firstName, appleUserReference: appleUserReference)
+            
+            let userRecord = CKRecord(user: newUser)
+            
+            self.publicDB.save(userRecord, completionHandler: { (_, error) in
+                
+                if let error = error {
+                    print("Save error \(error.localizedDescription)")
+                    completion(false); return
+                }
+            
+                self.currentUser = newUser
+                
+                completion(true)
+            })
         }
     }
 }
